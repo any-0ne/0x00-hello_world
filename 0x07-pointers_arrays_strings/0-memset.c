6ca56fb543331e7f8732d7affa548d26f700ba6f
any-One <oussamakouraich@gmail.com>
2023-07-05 02:05:39 +0100

    memset again

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,9 +4,9 @@
  * @s: memory area
  * @b: constant byte
  * @n: number of bytes
- * Return: the pointer to s.
+ * Return: pointer to s.
  */
-char* _memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
