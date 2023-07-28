07254b3249c6be67dd026f4b4d3a7efc86ed5c58
any-One <oussamakouraich@gmail.com>
2023-07-28 02:29:07 +0100

    print singely linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_list - prints a list_t list
+ * print_list - printsprints all the elements of  a list_t list
  * @h: singly linked list
  * Return: number of elements in the list.
  */
