9668e2391110a6ed80133ecbba6d5577f59c8ba9
any-One <oussamakouraich@gmail.com>
2023-07-28 19:57:48 +0100

    Fix assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,16 @@
 section .data
 	hello db "Hello, Holberton", 10
-
 section .text
 	extern printf
 	global main
 
 main:
-    ; Set up the stack frame
 	push rbp
 	mov rbp, rsp
 
-    ; Prepare the arguments for printf
 	mov rdi, hello
 	mov eax, 0
 	call printf
 
-    ; Restore the stack frame and return
-    pop rbp
-    ret
-
+	pop rbp
+	ret
