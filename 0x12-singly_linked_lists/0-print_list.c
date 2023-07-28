78341215bd1d05e4cec77f0d55beb5c667cdc361
any-One <oussamakouraich@gmail.com>
2023-07-28 02:21:26 +0100

    print singely linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,18 +6,18 @@
  * Return: number of elements in the list
  */
 
-size_t print_list(const list_t *myList)
+size_t print_list(const list_t *h)
 {
 	size_t nbElem;
 
 	nbElem = 0;
-	while (myList != NULL)
+	while (h != NULL)
 	{
-		if (myList->str == NULL)
+		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", myList->len, myList->str);
-		myList = myList->next;
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 		nbElem++;
 	}
 	
