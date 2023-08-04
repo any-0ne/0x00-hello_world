7a254f30eb4e2cebf215fdb4f82d57d10f7d19be
any-One <oussamakouraich@gmail.com>
2023-08-04 01:15:18 +0100

    Correction PUSH

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,7 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int mask = 1UL << index;
 
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 
 	return ((n & mask) ? 1 : 0);
 }
